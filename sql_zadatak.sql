b651cff6fec81c8f1cfefb1df07720f05f035cb5
danijelkelava <dykyprod@gmail.com>
2017-12-21 15:56:37 +0100

    queries za najstarije clanove

--- sql_zadatak.sql
+++ sql_zadatak.sql
@@ -17,3 +17,32 @@ create table crew_experience (
     aircraft_id SERIAL NOT NULL REFERENCES aircrafts(id),
     PRIMARY KEY(crew_member_id, aircraft_id)
 );
+
+insert into crew_members values (default, 'LUKE', 'SKYWALKER', '1983-05-22'), 
+	                            (default, 'ANAKIN', 'SKYWALKER', '1963-07-12'),
+	                            (default, 'HAN', 'SOLO', '1977-08-06'),
+	                            (default, 'OBI-WAN', 'KENOBI', '1953-10-29');
+
+insert into aircrafts values (default, 'A-wing'),
+	                         (default, 'ARC-170'),
+	                         (default, 'B-wing'),
+	                         (default, 'E-wing'),
+	                         (default, 'Hornet Interceptor'),
+	                         (default, 'Naboo N-1'),
+	                         (default, 'X-wing');
+	                         
+insert into crew_experience values (1,1),(1,2),(1,3),(1,4),(1,5),(1,6),(1,7),
+	                               (3,1),(3,2),(3,3),(3,4),(3,5),(3,6),
+	                               (4,1),(4,2),(4,3),(4,4),
+	                               (5,1),(5,2),(5,3),(5,4),(5,5);
+
+
+#query za ime najstarijeg clana posade
+SELECT first_name, last_name FROM crew_members ORDER BY date_of_birth ASC LIMIT 1;
+
+#query za nth crew_member 
+SELECT first_name, last_name, date_of_birth FROM crew_members ORDER BY date_of_birth ASC LIMIT 1 OFFSET 2;
+
+#kako je prvi najstariji na poziciji 0, da nam bude lakse mozemo zapisati OFFSET (nth-1) i na mjesto nth zapisemo 
+#kojeg po redu najstarijeg clana zelimo 
+SELECT first_name, last_name, date_of_birth FROM crew_members ORDER BY date_of_birth ASC LIMIT 1 OFFSET (nth-1);
