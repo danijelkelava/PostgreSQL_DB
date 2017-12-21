4df598074c35a094aa45a833b996e1b9ffec246b
danijelkelava <dykyprod@gmail.com>
2017-12-21 15:52:28 +0100

    kod za bazu podataka i tabelu crew_members

---
+++ sql_zadatak.sql
@@ -0,0 +1,8 @@
+create database air_company;
+
+create table crew_members (
+    id SERIAL UNIQUE PRIMARY KEY NOT NULL,
+    first_name varchar(255) NOT NULL,
+    last_name varchar(255) NOT NULL,
+    date_of_birth date NOT NULL
+);
